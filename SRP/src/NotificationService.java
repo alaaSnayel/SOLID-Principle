fc441bb538a02654abd4366d35d20a467a5b2a6a
alaaSnayel <alaanayel111@gmail.com>
2025-12-16 14:21:40 +0200

    Single Resposibilety principle

---
+++ SRP/src/NotificationService.java
@@ -0,0 +1,12 @@
+
+public class NotificationService {
+    public void sendOTP(String medium) {
+        if (medium.equals("email")) {
+            //write email related logic
+            //use JavaMailSenderAPI
+        }
+        if(medium.equals("mobile")){
+            //write logic using twillio API
+        }
+    }
+}
